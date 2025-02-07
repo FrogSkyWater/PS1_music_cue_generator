0c15aec8e9009aa14ef4950ae61b34dacd487d98
Maël <embe44160@gmail.com>
2025-02-07 14:30:05 +0100

    Update playlist_maker.bat

--- playlist_maker.bat
+++ playlist_maker.bat
@@ -1,16 +1,12 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Nom du fichier de sortie
 set OUTPUT_FILE=playlist.cue
 
-:: Supprime le fichier s'il existe déjà
 if exist %OUTPUT_FILE% del %OUTPUT_FILE%
 
-:: Initialisation du compteur de piste
 set TRACK_NUM=1
 
-:: Boucle sur chaque fichier donné en argument
 for %%F in (*.wav) do (
     echo FILE "%%F" WAVE >> %OUTPUT_FILE%
     echo   TRACK !TRACK_NUM! AUDIO >> %OUTPUT_FILE%
